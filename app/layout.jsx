8e1b9aef9a430af1e8efec883602d2046e90e2bb
psycheverity-jpg <psycheverity@gmail.com>
2026-08-09 15:20:56 +0700

    Add files via upload

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="md:cursor-none">
         <SmoothScroll>{children}</SmoothScroll>
